82c581c9db366ccfe84e67f88ccfadbb974108d1
dayunhan <dayun.dev@gmail.com>
2020-03-17 00:38:45 +0900

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Araut\Web-Application\dayunhan.pythonanywhere.com\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
