cc48daa4488d0f28c5b97133d3ad704ea913a547
dayunhan <dayun.dev@gmail.com>
2020-03-18 15:54:36 +0900

    Create post_new view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from .models import Post
+from .forms import PostForm
 
 from django.shortcuts import render
 from django.utils import timezone
@@ -11,3 +12,8 @@ def index(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
