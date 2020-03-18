430cfe8f16008510497c5dc730a8022589b654d9
dayunhan <dayun.dev@gmail.com>
2020-03-18 13:00:06 +0900

    Create post_list view

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'blog/index.html', {})
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
