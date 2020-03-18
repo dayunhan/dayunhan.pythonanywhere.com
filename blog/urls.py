b484111e1e5d9f4be40671940c4bee771ceb505d
dayunhan <dayun.dev@gmail.com>
2020-03-18 15:56:04 +0900

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.post_list, name='post_list'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
