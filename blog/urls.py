ba6743e2a4b8734279bf5fc452f7f47c99f2d620
dayunhan <dayun.dev@gmail.com>
2020-03-19 13:09:59 +0900

    Add post_detail path

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.post_list, name='post_list'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
