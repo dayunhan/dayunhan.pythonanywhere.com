de712d708c5cb643a1d9a6c3d17b59df72a22c10
dayunhan <dayun.dev@gmail.com>
2020-03-19 13:24:27 +0900

    Go to the detail page of the created post

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .forms import PostForm
 
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 
 
@@ -23,6 +24,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm
     return render(request, 'blog/post_edit.html', {'form': form})
